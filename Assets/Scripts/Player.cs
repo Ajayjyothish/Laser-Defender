33957197535e29a6fb3fed01055e95807d42760b
LAPTOP-HT0THONL\Ajay <ajayjyothish007@gmail.com>
2018-08-23 16:00:43 +0530

    Laser Destroyer and ememy waypoints set up

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
+    //config parameters
     [SerializeField] float PlayerSpeed =1f;
     [SerializeField] float BoundaryPadding = 1f;
+    [SerializeField] GameObject PlayerLaser;
+    [SerializeField] float LaserSpeed = 5f;
+    [SerializeField] float LaserDelay;
+
+
+    GameObject laser;
     float xMaxBound,xMinBound,yMinBound,yMaxBound;
+    Coroutine fireContinuously;
     
 	void Start () {
         SetUpMoveBoundaries();
@@ -20,13 +27,38 @@ public class Player : MonoBehaviour {
         xMaxBound = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
         yMinBound = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
         yMaxBound = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
-
-        
-
     }
 
     void Update () {
         Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+             fireContinuously= StartCoroutine(FireAndWait());
+           
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(fireContinuously);
+            
+        }
+    }
+
+    IEnumerator FireAndWait()
+    {
+        while (true)
+        {
+            laser = Instantiate(PlayerLaser, transform.position, transform.rotation);
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, LaserSpeed);
+            yield return new WaitForSeconds(LaserDelay);
+        }
     }
 
     private void Move()
