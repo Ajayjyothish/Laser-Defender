e5229ca3b52593361fcc15254c330bbaabc56ff9
LAPTOP-HT0THONL\Ajay <ajayjyothish007@gmail.com>
2018-08-23 23:42:23 +0530

    Enemy laser Setup

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var missile = collision.gameObject.GetComponent<LaserHit>();
+        if (missile)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/EnemyPath.cs
+++ Assets/Scripts/EnemyPath.cs
@@ -1,24 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPath : MonoBehaviour {
     [SerializeField] List<Transform> enemyPath;
     [SerializeField] float speed;
+    [SerializeField] GameObject enemy;
+    [SerializeField] float ShootDelay;
+    [SerializeField] GameObject laser;
+    [SerializeField] float LaserSpeed;
 
     int enemyPathIndex = 0;
-    
+    GameObject enemyShip;
+
     // Use this for initialization
-    void Start () {
-        transform.position = enemyPath[enemyPathIndex].position;
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    void Start ()
+    {
+        GetStartLocation();
+        FireLaser();
+    }
+
+   
+    void Update ()
+    {
+        FollowPath();
+
+    }
+
+
+    private void FireLaser()
+    {
+        InvokeRepeating("ShootAndWait",0.000001f,ShootDelay);
+    }
+
+    void ShootAndWait()
+    {
+        var lasers=  Instantiate(laser, transform.position, Quaternion.identity);
+        lasers.GetComponent<Rigidbody2D>().velocity = Vector2.down * LaserSpeed;
+    }
+   
+
+    
+    private void GetStartLocation()
+    {
+        enemyShip = Instantiate(enemy, enemyPath[enemyPathIndex].position, transform.rotation);
+        enemyShip.transform.parent = transform;
+    }
+    private void FollowPath()
     {
         if (enemyPathIndex <= enemyPath.Count - 1)
         {
+
             var targetLocation = enemyPath[enemyPathIndex].position;
             var enemySpeed = speed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetLocation, enemySpeed);
@@ -27,10 +60,8 @@ public class EnemyPath : MonoBehaviour {
                 enemyPathIndex++;
 
         }
+
         else
             Destroy(gameObject);
-
-
-		
-	}
+    }
 }
